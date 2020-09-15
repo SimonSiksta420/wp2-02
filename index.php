810ec15ebe93d8db876585d5ad873afc42dbac71
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-15 15:32:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+ $submit = filter_input(INPUT_POST, 'submit');
+ $spz = filter_input(INPUT_POST, 'spz');
+ $auto = filter_input(INPUT_POST, 'auto');
+ $stariauta = filter_input(INPUT_POST, 'stariauta');
+ $types = array('Auto', 'Autobus','Dodávka','Nákladní automobil');
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Prodej aut</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+<?php
+if (isset($submit)) { ?>
+  <p>Auto: <?= $auto ?></p>
+  <p>SPZ: <?= $spz ?></p>
+  <p>Stáří auta: <?= $stariauta ?>
+  <?php
+} else {
+  ?> 
+  <form action="wp2-1.php" method="post">
+ 
+ Rok výroby: <input type="number" id="stari" name="stariauta" min="1900" max="2020">
+
+ Typ vozidla: <select name="auto" id="cars">
+ <?php foreach ($types as $type) { ?>
+   <option value="<?= $type ?>"><?= $type ?> </option>
+ <?php } ?>
+
+ ?>
+</select>
+<br>SPZ <input type="text" name="spz" > 
+<br> <input type="submit" name="submit" value="Odeslat">
+</form>
+<?php } ?>
+</form>
+</body>
+</html>
