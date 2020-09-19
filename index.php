b975daca0bc70fa59b58b2c7cdbd862a27b1a3e8
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-19 10:34:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Prodej aut</title>
+    <title> Prodej aut </title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
@@ -19,9 +19,9 @@
 
 <?php
 if (isset($submit)) { ?>
-  <p>Auto: <?= $auto ?></p>
-  <p>SPZ: <?= $spz ?></p>
-  <p>Stáří auta: <?= $stariauta ?>
+  <p> Auto: <?= $auto ?> </p>
+  <p> SPZ: <?= $spz ?> </p>
+  <p> Stáří auta: <?= $stariauta ?> </p>
 
   <form action="index.php" method="post">
  
